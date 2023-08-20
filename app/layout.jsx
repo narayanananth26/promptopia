926a29e46af51edd75e7c5f301cab6ab5d161be8
Ananth Narayan <narayanananth26@gmail.com>
2023-08-20 12:15:32 +0530

    create root layout

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+	title: "Promptopia",
+	description: "Discover & Share AI Prompts",
+};
+
+function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body>
+				<div className="main">
+					<div className="gradient" />
+					<main className="app">{children}</main>
+				</div>
+			</body>
+		</html>
+	);
+}
+
+export default RootLayout;
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+// * RENDERS HOME PAGE
+
+function Home() {
+	return <div>Home</div>;
+}
+
+export default Home;
