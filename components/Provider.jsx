40f1f2fafdaa0f072f9329cd2daccb59502ea3e8
Ananth Narayan <narayanananth26@gmail.com>
2023-08-21 20:48:46 +0530

    create session provider

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,5 +1,7 @@
-function Provider() {
-	return <div>Provider</div>;
+import { SessionProvider } from "next-auth/react";
+
+function Provider({ children, session }) {
+	return <SessionProvider session={session}>{children}</SessionProvider>;
 }
 
 export default Provider;
