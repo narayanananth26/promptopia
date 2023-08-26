941f71821b4849c24eabded19f6662c8c0aa692a
Ananth Narayan <narayanananth26@gmail.com>
2023-08-26 11:14:52 +0530

    create root layout structure

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,8 +6,18 @@ export const metadata = {
 	description: "Discover & Share AI Prompts",
 };
 
-function RootLayout() {
-	return <div></div>;
+function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body>
+				<div className="main">
+					<div className="gradient" />
+				</div>
+
+				<main className="app">{children}</main>
+			</body>
+		</html>
+	);
 }
 
 export default RootLayout;
