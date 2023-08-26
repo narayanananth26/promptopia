3f7275674426e0e8cf61750ed636489da57abe73
Ananth Narayan <narayanananth26@gmail.com>
2023-08-26 11:21:57 +0530

    add & style heading & desc to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,21 @@
 function Home() {
-	return <div></div>;
+	return (
+		<section className="w-full flex-center flex-col">
+			<h1 className="head_text text-center">
+				Discover & Share
+				<br className="max-md:hidden" />
+				<span className="orange_gradient text-center">
+					AI-Powered Prompts
+				</span>
+			</h1>
+			<p className="desc text-center">
+				Promptopia is an open-source AI prompting tool for modern world
+				to discover and share creative prompts
+			</p>
+
+			{/* Feed */}
+		</section>
+	);
 }
 
 export default Home;
