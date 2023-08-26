79e36179ba4c9b2477764b9665ca0a5b8813dc98
Ananth Narayan <narayanananth26@gmail.com>
2023-08-26 12:16:04 +0530

    add logo to navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+
 function Nav() {
-	return <div>Nav</div>;
+	return (
+		<nav className="flex-between w-full mb-16 pt-3">
+			<Link href="/" className="flex gap2 flex-center">
+				<Image
+					src="/assets/images/logo.svg"
+					alt="Promptopia logo"
+					width={30}
+					height={30}
+					className="object-contain"
+				/>
+				<p className="logo_text">Promptopia</p>
+			</Link>
+		</nav>
+	);
 }
 
 export default Nav;
