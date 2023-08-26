e6e8fa841c7af2ec65d39ce7e0e10c8efcb9bbac
Ananth Narayan <narayanananth26@gmail.com>
2023-08-26 11:12:33 +0530

    provide static metadata to root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import "@styles/globals.css";
 
+// Static metadata
+export const metadata = {
+	title: "Promptopia",
+	description: "Discover & Share AI Prompts",
+};
+
 function RootLayout() {
 	return <div></div>;
 }
