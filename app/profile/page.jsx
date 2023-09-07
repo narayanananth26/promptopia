19ded6f8697d7a19f8a1a22da9729180a77eec87
Ananth Narayan <narayanananth26@gmail.com>
2023-09-07 12:06:30 +0530

    create and style profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,5 +1,38 @@
-const Profile = () => {
-	return <div>Profile</div>;
+"use client";
+
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+const MyProfile = () => {
+	const { data: session } = useSession();
+
+	const [posts, setPosts] = useState("");
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch(`/api/users/${session.user.id}/posts`);
+			const data = await response.json();
+
+			setPosts(data);
+		};
+		if (session?.user.id) fetchPosts();
+	}, [session.user.id]);
+
+	const handleEdit = () => {};
+	const handleDelete = async () => {};
+
+	return (
+		<Profile
+			name="My"
+			desc="Welcome to your personalized profile page"
+			data={[]}
+			handleEdit={handleEdit}
+			handleDelete={handleDelete}
+		>
+			Profile
+		</Profile>
+	);
 };
 
-export default Profile;
+export default MyProfile;
